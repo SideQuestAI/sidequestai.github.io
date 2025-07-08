a6e0e9068dc858e76fac27b39078960e88d0b008
SideQuestAI <sidequestaiofficial@gmail.com>
2025-07-08 20:40:54 +0530

    Fix plan button to redirect to Gumroad, update config for plan links

--- src/config/app.config.ts
+++ src/config/app.config.ts
@@ -36,7 +36,7 @@ export const appConfig = {
     android: {
       link: "https://github.com/SideQuestAI/sidequestai.github.io/blob/main/SideQuestAI.android.apk", // "https://play.google.com/store/apps/details?id=com.sidequestai" (page redirect)
       //        or "https://github.com/user/repo/releases/download/v1.0.0/app.apk" (direct download)
-      version: "v0.5",
+      version: "0.5",
     },
 
     // Desktop - Direct file downloads from GitHub releases or CDN
@@ -51,7 +51,7 @@ export const appConfig = {
     linux: {
       link: "https://github.com/SideQuestAI/sidequestai.github.io/blob/main/sidequestai", // "https://github.com/username/sidequestai/releases/download/v1.0.0/SideQuestAI.AppImage" (direct download)
       //        or "https://github.com/user/repo/releases/download/v1.0.0/sidequestai.deb" (direct download)
-      version: "v0.5",
+      version: "0.5",
     },
 
     // Web - Page redirect
